2fa1e5bb4b2914e46616ce6c7f88c56ccc4bfb8f
rhiga2 <ryley.higa2@gmail.com>
2018-09-20 00:23:05 -0500

    Implementing binarized networks

---
+++ bitwise_network.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Function
+import glob
+import numpy as np
+from bss_eval import *
+from torch.utils.data import Dataset, DataLoader
+from two_source_mixture import *
+from binary_data import *
+import argparse
+
+class BitwiseNetwork(nn.Module):
+    def __init__(self, input_size, output_size, fc_sizes = [], dropout=0, activation=torch.tanh,
+    constrainer=torch.tanh):
+        super(BitwiseNetwork, self).__init__()
+        self.params = {}
+        self.num_layers = len(fc_sizes) + 1
+        fc_sizes = fc_sizes + [output_size,]
+        in_size = input_size
+        self.constrainer = constrainer
+        self.dropout_list = nn.ModuleList()
+        for i, out_size in enumerate(fc_sizes):
+            wname, bname = 'weight%d' % (i+1,), 'bias%d' % (i+1,)
+            w = torch.empty(out_size, in_size)
+            nn.init.xavier_uniform_(w)
+            b = torch.zeros(out_size)
+            in_size = out_size
+            setattr(self, wname, nn.Parameter(w, requires_grad=True, name=wname))
+            setattr(self, bname, nn.Parameter(b, requires_grad=True, name=bname))
+            self.dropout_list.append(nn.Dropout(dropout))
+        self.activation = activation
+
+    def forward(self, x):
+        '''
+        * Input is a tensor of shape (N, F, T)
+        * Output is a tensor of shape (N, F, T)
+        '''
+        # Flatten (N, F, T) -> (NT, F)
+        h = x.permute(0, 2, 1).contiguous().view(-1, x.size(1))
+        for i in range(self.num_layers):
+            wname, bname = 'weight%d' % (i+1,), 'bias%d' % (i+1,)
+            modified_w = torch.tanh(getattr(self, wname))
+            modified_b = torch.tanh(getattr(self, bname))
+            h = F.linear(h, modified_w, modified_b)
+            if i != self.num_layers:
+                h = self.activation(h)
+        # Unflatten (NT, F) -> (N, F, T)
+        y = h.view(x.size(0), x.size(2), -1).permute(0, 2, 1)
+        return y
+
+    def contrain_params(self):
+        for param in self.parameters():
+            pass
+
+
+class BinarizePreactivations(Function):
+    @staticmethod
+    def forward(ctx, x):
+        ctx.save_for_backward(x)
+        return torch.sign(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        x = ctx.saved_tensors
+        return grad_output * (1 - torch.tanh(x)**2)
+
+class BinarizeParams(Function):
+    @staticmethod
+    def forward(ctx, x, beta):
+        return torch.tensor((x > beta) - (x < beta), dtype=x.dtype)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output, None
+
+binarize_preactivations = BinarizePreactivations.apply
+binarize_params = BinarizeParams.apply
+
+def make_dataset(batchsize, seed=0):
+    np.random.seed(seed)
+    trainset = BinaryDataset('/media/data/binary_audio/train')
+    valset = BinaryDataset('/media/data/binary_audio/val')
+    collate_fn = lambda x: collate_and_trim(x, axis=1)
+    train_dl = DataLoader(trainset, batch_size=batchsize, shuffle=True, collate_fn=collate_fn)
+    val_dl = DataLoader(valset, batch_size=batchsize, collate_fn=collate_fn)
+    return train_dl, val_dl
+
+def main():
+    parser = argparse.ArgumentParser(description='bitwise network')
+    parser.add_argument('--epochs', '-e', type=int, default=64,
+                        help='Number of epochs')
+    parser.add_argument('--batchsize', '-b', type=int, default=16,
+                        help='Training batch size')
+    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-3)
+    parser.add_argument('--weight_decay', '-wd', type=float, default=0)
+    parser.add_argument('--dropout', '-dropout', type=float, default=0.2)
+    args = parser.parse_args()
+
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+
+    train_dl, val_dl = make_dataset(args.batchsize)
+    model = BitwiseNetwork(2052, 513, fc_sizes=[1024, 1024], dropout=args.dropout).to(device)
+    print(model)
+    loss = torch.nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+
+    def model_loss(model, binary_batch, compute_bss=False):
+        bmag, ibm = batch['bmag'].cuda(device), batch['ibm'].cuda(device)
+        premask = model(2*bmag-1)
+        cost = loss(premask, ibm)
+        return cost
+
+    print('Real Network Training')
+    for epoch in range(args.epochs):
+        total_cost = 0
+        count = 0
+        bss_metrics = BSSMetricsList()
+        model.train()
+        for count, batch in enumerate(train_dl):
+            optimizer.zero_grad()
+            cost = model_loss(model, batch)
+            total_cost += cost.data
+            cost.backward()
+            optimizer.step()
+        avg_cost = total_cost / (count + 1)
+
+        if epoch % 8 == 0:
+            print('Epoch %d Training Cost: ' % epoch, avg_cost)
+
+            total_cost = 0
+            bss_metrics = BSSMetricsList()
+            model.eval()
+            for count, batch in enumerate(val_dl):
+                cost = model_loss(model, batch)
+                total_cost += cost.data
+            avg_cost = total_cost / (count + 1)
+            print('Validation Cost: ', avg_cost)
+            torch.save(model.state_dict(), 'models/real_network.model')
+
+    print('Noisy Training')
+
+
+if __name__ == '__main__':
+    main()
