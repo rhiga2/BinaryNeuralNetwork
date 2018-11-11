a2546f72b2be2a2acd4ddc5b672c897c4f119184
Ryley Higa <ryley.higa2@gmail.com>
2018-11-11 00:31:38 -0600

    Fixed minor bug involving bad indentation

--- dnn/bitwise_ss.py
+++ dnn/bitwise_ss.py
@@ -117,75 +117,75 @@ class BitwiseNetwork(nn.Module):
                 optimizer.step()
         return running_loss / len(dl.dataset)
 
-    def main():
-        parser = argparse.ArgumentParser(description='bitwise network')
-        parser.add_argument('--epochs', '-e', type=int, default=64,
-                            help='Number of epochs')
-        parser.add_argument('--batchsize', '-b', type=int, default=8,
-                            help='Training batch size')
-        parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
-        parser.add_argument('--lr_decay', '-lrd', type=float, default=1.0)
-        parser.add_argument('--weight_decay', '-wd', type=float, default=0)
-        parser.add_argument('--dropout', '-dropout', type=float, default=0.2)
-        parser.add_argument('--train_noisy', '-tn',  action='store_true')
-        parser.add_argument('--output_period', '-op', type=int, default=1)
-        parser.add_argument('--load_file', '-lf', type=str, default=None)
-        parser.add_argument('--sparsity', '-sparsity', type=float, default=0)
-        parser.add_argument('--l1_reg', '-l1r', type=float, default=0)
-        parser.add_argument('--toy', action='store_true')
-        parser.add_argument('--model_file', '-mf', default='temp_model.model')
-        args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='bitwise network')
+    parser.add_argument('--epochs', '-e', type=int, default=64,
+                        help='Number of epochs')
+    parser.add_argument('--batchsize', '-b', type=int, default=8,
+                        help='Training batch size')
+    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
+    parser.add_argument('--lr_decay', '-lrd', type=float, default=1.0)
+    parser.add_argument('--weight_decay', '-wd', type=float, default=0)
+    parser.add_argument('--dropout', '-dropout', type=float, default=0.2)
+    parser.add_argument('--train_noisy', '-tn',  action='store_true')
+    parser.add_argument('--output_period', '-op', type=int, default=1)
+    parser.add_argument('--load_file', '-lf', type=str, default=None)
+    parser.add_argument('--sparsity', '-sparsity', type=float, default=0)
+    parser.add_argument('--l1_reg', '-l1r', type=float, default=0)
+    parser.add_argument('--toy', action='store_true')
+    parser.add_argument('--model_file', '-mf', default='temp_model.model')
+    args = parser.parse_args()
 
-        # Initialize device
-        dtype = torch.float32
-        if torch.cuda.is_available():
-            device = torch.device('cuda:0')
-        else:
-            device = torch.device('cpu')
-        print('On device: ', device)
+    # Initialize device
+    dtype = torch.float32
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+    print('On device: ', device)
 
-        # Make model and dataset
-        train_dl, val_dl = make_binary_data(args.batchsize, toy=args.toy)
-        model = BitwiseNetwork(in_size=2048, out_size=512, fc_sizes=[2048, 2048],
-            dropout=args.dropout, sparsity=args.sparsity, autoencode=args.autoencode)
-        if args.train_noisy:
-            print('Noisy Network Training')
-            if args.load_file:
-                model.load_state_dict(torch.load('../models/' + args.load_file))
-            model.noisy()
-        else:
-            print('Real Network Training')
-        model.to(device=device)
-        print(model)
+    # Make model and dataset
+    train_dl, val_dl = make_binary_data(args.batchsize, toy=args.toy)
+    model = BitwiseNetwork(in_size=2048, out_size=512, fc_sizes=[2048, 2048],
+        dropout=args.dropout, sparsity=args.sparsity, autoencode=args.autoencode)
+    if args.train_noisy:
+        print('Noisy Network Training')
+        if args.load_file:
+            model.load_state_dict(torch.load('../models/' + args.load_file))
+        model.noisy()
+    else:
+        print('Real Network Training')
+    model.to(device=device)
+    print(model)
 
-        # Initialize loss function
-        loss = nn.BCEWithLogitsLoss()
-        loss = loss.to(device=device)
-        metrics = LossMetrics()
+    # Initialize loss function
+    loss = nn.BCEWithLogitsLoss()
+    loss = loss.to(device=device)
+    metrics = LossMetrics()
 
-        # Initialize optimizer
-        vis = visdom.Visdom(port=5800)
-        lr = args.learning_rate
-        optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=args.weight_decay)
+    # Initialize optimizer
+    vis = visdom.Visdom(port=5800)
+    lr = args.learning_rate
+    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=args.weight_decay)
 
-        for epoch in range(args.epochs):
-            total_cost = 0
-            model.update_betas()
-            model.train()
-            train_loss = train(model, train_dl, optimizer, loss=loss, device=device)
+    for epoch in range(args.epochs):
+        total_cost = 0
+        model.update_betas()
+        model.train()
+        train_loss = train(model, train_dl, optimizer, loss=loss, device=device)
 
-            if epoch % args.output_period == 0:
-                print('Epoch %d Training Cost: ' % epoch, train_loss)
-                model.eval()
-                val_loss = val(model, val_dl, loss=loss, device=device)
-                print('Val Cost: ', val_loss)
-                metrics.update(train_loss, val_loss,
-                    output_period=args.output_period)
-                train_plot(vis, loss_metrics, eid='Ryley', win=['Loss', None])
-                torch.save(model.state_dict(), '../models/' + args.model_file)
-                lr *= args.lr_decay
-                optimizer = optim.Adam(model.parameters(), lr=lr,
-                    weight_decay=args.weight_decay)
+        if epoch % args.output_period == 0:
+            print('Epoch %d Training Cost: ' % epoch, train_loss)
+            model.eval()
+            val_loss = val(model, val_dl, loss=loss, device=device)
+            print('Val Cost: ', val_loss)
+            metrics.update(train_loss, val_loss,
+                output_period=args.output_period)
+            train_plot(vis, loss_metrics, eid='Ryley', win=['Loss', None])
+            torch.save(model.state_dict(), '../models/' + args.model_file)
+            lr *= args.lr_decay
+            optimizer = optim.Adam(model.parameters(), lr=lr,
+                weight_decay=args.weight_decay)
 
 if __name__ == '__main__':
     main()
