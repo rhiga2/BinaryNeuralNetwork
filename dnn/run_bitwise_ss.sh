159edc54ce4637b470bfa2ea49b20b8875ac5473
Ryley Higa <ryley.higa2@gmail.com>
2019-01-22 13:00:37 -0600

    Added updates to experiment files

--- dnn/run_bitwise_ss.sh
+++ dnn/run_bitwise_ss.sh
@@ -1,4 +1,5 @@
 python bitwise_ss.py -exp net_tanh -a identity -wb tanh -ib tanh -ub -lr 1e-4 -e 256
 python bitwise_ss.py -exp net_nocw_cg_tanh -a identity -wb clipped_ste -ib clipped_ste -ub -lf net_tanh.model -lr 1e-5 -e 256
-python bitwise_ss.py -exp net_cw_cg_tanh -a identity -wa clipped_ste -ib clipped_ste -ub -lf net_tanh.model -cw -lr 1e-5 -e 256
-python bitwise_ss.py -exp net_cw_nocg_tanh -a identity -wa ste -ib ste -ub -lf net_tanh.model -cw -lr 1e-5 -e 256
+python bitwise_ss.py -exp net_cw_cg_tanh -a identity -wb clipped_ste -ib clipped_ste -ub -lf net_tanh.model -cw -lr 1e-5 -e 256
+python bitwise_ss.py -exp net_cw_nocg_tanh -a identity -wb ste -ib ste -ub -lf net_tanh.model -cw -lr 1e-5 -e 256
+python bitwise_ss.py -exp -a identity -wb ste -ib ste -ub -lf net_tanh.model -lr 1e-5 -e 256
