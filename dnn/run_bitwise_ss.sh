0befcd4e75ad1acbbdb1156c01541043e1e00e49
Ryley Higa <higa2@illinois.edu>
2019-01-23 01:47:28 -0600

    Fixed minor bug

--- dnn/run_bitwise_ss.sh
+++ dnn/run_bitwise_ss.sh
@@ -1,5 +1,5 @@
-python bitwise_ss.py -exp net_tanh -a identity -wb tanh -ib tanh -ub -lr 1e-4 -e 256
-python bitwise_ss.py -exp net_nocw_cg_tanh -a identity -wb clipped_ste -ib clipped_ste -ub -lf net_tanh.model -lr 1e-5 -e 256
-python bitwise_ss.py -exp net_cw_cg_tanh -a identity -wb clipped_ste -ib clipped_ste -ub -lf net_tanh.model -cw -lr 1e-5 -e 256
-python bitwise_ss.py -exp net_cw_nocg_tanh -a identity -wb ste -ib ste -ub -lf net_tanh.model -cw -lr 1e-5 -e 256
-python bitwise_ss.py -exp net_nocw_nocg_tanh -a identity -wb ste -ib ste -ub -lf net_tanh.model -lr 1e-5 -e 256
+python bitwise_ss.py -exp net_tanh -a identity -wb tanh -ib tanh -ub -lr 1e-3 -e 64
+python bitwise_ss.py -exp net_nocw_cg_tanh -a identity -wb bitwise_activation -ib bitwise_activation -ub -lf net_tanh.model -lr 1e-4 -e 64 -dropout 0
+python bitwise_ss.py -exp net_cw_cg_tanh -a identity -wb bitwise_activation -ib bitwise_activation -ub -lf net_tanh.model -cw -lr 1e-4 -e 64 -dropout 0
+python bitwise_ss.py -exp net_cw_nocg_tanh -a identity -wb ste -ib ste -ub -lf net_tanh.model -cw -lr 1e-4 -e 64 -dropout 0
+python bitwise_ss.py -exp net_nocw_nocg_tanh -a identity -wb ste -ib ste -ub -lf net_tanh.model -lr 1e-4 -e 64 -dropout 0
