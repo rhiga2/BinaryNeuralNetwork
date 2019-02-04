de44cbf714b0f5d49caec129b11775af5fc55a42
Ryley Higa <ryley.higa2@gmail.com>
2019-02-04 10:16:12 -0600

    Added wsj0 to the mix

--- datasets/make_data.py
+++ datasets/make_data.py
@@ -4,60 +4,41 @@ sys.path.append('../')
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-from datasets.two_source_mixture import *
+import datasets.two_source_mixture as two_source_mixture
 import argparse
 
-def make_mixture_set(hop=256, toy=False, max_duration=None, transform=None):
-    speaker_path = '/media/data/timit-wav/train'
-    targets = ['dr1/fcjf0', 'dr1/fetb0', 'dr1/fsah0', 'dr1/fvfb0',
-        'dr1/fdaw0', 'dr1/fjsp0','dr1/fsjk1', 'dr1/fvmh0',
-        'dr1/fsma0', 'dr1/ftbr0']
-    train_speeches, val_speeches, test_speeches = get_speech_files(
-        speaker_path,
-        targets,
-        num_train=6,
-        num_val=2
-    )
+def make_mixture_set(hop=256, toy=False, max_duration=None, transform=None,
+    timit=False):
 
-    if toy:
-        noise_path = '/media/data/Nonspeech'
-        interferences = ['n81.wav', # chimes
-                         'n97.wav', # eating chips
-                         'n21.wav', # motorcycle
-                         'n46.wav', # ocean
-                         'n47.wav', # birds
-                         'n55.wav', # cicadas?
-                         'n59.wav', # jungle?
-                         ]
-        train_noises, val_noises, test_noises = get_noise_files(
-            noise_path,
-            interferences,
-            num_train=4,
-            num_val=2,
-            num_test=1
-        )
-        trainset = TwoSourceMixtureDataset(train_speeches, train_noises, hop=hop,
-            max_duration=max_duration, transform=transform)
-        valset = TwoSourceMixtureDataset(val_speeches, val_noises, hop=hop,
-            max_duration=max_duration, transform=transform)
-        testset = TwoSourceMixtureDataset(test_speeches, test_noises, hop=hop,
-            max_duration=max_duration, transform=transform)
-    else:
-        interferences = ['dr1/mdpk0', 'dr1/mjwt0', 'dr1/mrai0', 'dr1/mrws0',
+    if timit:
+        directory = '/media/data/timit-wav/train'
+        targets = ['dr1/fcjf0', 'dr1/fetb0', 'dr1/fsah0', 'dr1/fvfb0',
+            'dr1/fdaw0', 'dr1/fjsp0','dr1/fsjk1', 'dr1/fvmh0',
+            'dr1/fsma0', 'dr1/ftbr0']
+
+        inters = ['dr1/mdpk0', 'dr1/mjwt0', 'dr1/mrai0', 'dr1/mrws0',
             'dr1/mwad0', 'dr1/mwar0', 'dr1/mtrr0', 'dr1/mtjs0',
             'dr1/mcpm0', 'dr1/mmrp0']
-        train_noises, val_noises, test_noises = get_speech_files(
-            speaker_path,
-            interferences,
-            num_train=6,
-            num_val=2
-        )
-        trainset = TwoSourceMixtureDataset(train_speeches, train_noises, hop=hop,
-            max_duration=max_duration, transform=transform)
-        valset = TwoSourceMixtureDataset(val_speeches, val_noises, hop=hop,
-            max_duration=max_duration, transform=transform)
-        testset = TwoSourceMixtureDataset(test_speeches, test_noises, hop=hop,
-            max_duration=max_duration, transform=transform)
+    else:
+        directory = '/media/data/wsj/wsj0/11-1.1/wsj0/si_tr_s/'
+        targets = ['011', '014', '016', '017', '018', '019', '023', '027', '028', '1a', '1b']
+        inters = ['012', '013', '015', '020', '021', '022', '024', '025', '026', '029']
+
+    train_speech, val_speech, test_speech = two_source_mixture.get_speech_files(directory, targets,
+                                                                                train_percent=0.7,
+                                                                                val_percent=0.2,
+                                                                                max_utterances=30)
+    train_inter, val_inter, test_inter = two_source_mixture.get_speech_files(directory, inters,
+                                                                             train_percent=0.7,
+                                                                             val_percent=0.2,
+                                                                             max_utterances=30)
+
+    trainset = two_source_mixture.TwoSourceMixtureDataset(train_speech, train_inter, hop=hop,
+        max_duration=max_duration, transform=transform)
+    valset = two_source_mixture.TwoSourceMixtureDataset(val_speech, val_inter, hop=hop,
+        max_duration=max_duration, transform=transform)
+    testset = two_source_mixture.TwoSourceMixtureDataset(test_speech, test_inter, hop=hop,
+        max_duration=max_duration, transform=transform)
 
     return trainset, valset, testset
 
