72721f43e1473ccfa1e6123a043c469f6438587d
Ryley Higa <ryley.higa2@gmail.com>
2019-02-04 08:38:34 -0600

    Deleted constraints on num_train and num_val

--- datasets/two_source_mixture.py
+++ datasets/two_source_mixture.py
@@ -59,40 +59,6 @@ class TwoSourceMixtureDataset(Dataset):
         sigf, interf = self.mixes[i] # get sig and interference file
         return self._getmix(sigf, interf)
 
-class SineSpeechData(Dataset):
-    def __init__(self, speeches, num_sines, fs = 16000, noise_range = [3000, 8000], snr = 0,
-        hop=None):
-        self.fs = fs
-        self.hop = hop
-        self.num_sines = num_sines
-        self.snr = np.power(10, snr/20)
-        self.noise_freqs = np.random.uniform(noise_range[0], noise_range[1], num_sines)
-        self.noise_phases = np.random.random(num_sines) * 2*np.pi
-        sine_params = list(zip(self.noise_freqs, self.noise_phases))
-        self.mixes = list(itertools.product(speeches, sine_params))
-
-    def __getitem__(self, key):
-        # return (mixture, target, interference)
-        speech_file, sine_params = self.mixes[key]
-        freq, phase = sine_params
-        speech, _ = sf.read(speech_file)
-        if len(speech.shape) != 1:
-            speech = np.mean(speech, axis=1)
-
-        if self.hop:
-            speech = speech[:len(speech)//self.hop*self.hop]
-
-        time = np.arange(len(speech)) * 1 / self.fs
-        noise = np.sin(2*np.pi*freq * time + phase)
-        speech = speech / np.std(speech)
-        noise = noise / np.std(noise)
-        mix = speech + (1 / self.snr) * noise
-        mix = mix / np.std(mix)
-        return {'mixture': mix, 'target': speech, 'interference': noise}
-
-    def __len__(self):
-        return len(self.mixes)
-
 def collate_and_trim(batch, axis=0, hop=1, dtype=torch.float):
     keys = list(batch[0].keys())
     outbatch = {key: [] for key in keys}
@@ -108,7 +74,6 @@ def collate_and_trim(batch, axis=0, hop=1, dtype=torch.float):
     return outbatch
 
 def get_speech_files(speaker_path, speakers, num_train=8, num_val=2):
-    assert num_train + num_val <= 10 # Assume each speaker has 10 sentences
     if speaker_path[-1] != '/':
         speaker_path += '/'
     train_speeches = []
@@ -120,12 +85,15 @@ def get_speech_files(speaker_path, speakers, num_train=8, num_val=2):
             speaker += '/'
         files = glob.glob(speaker_path + speaker + '*.wav')
         train_speeches.extend(files[:num_train])
-        val_speeches.extend(files[num_train:num_train+num_val])
-        test_speeches.extend(files[num_train+num_val:])
+        end = num_train + num_val
+        if end > len(files):
+            end = len(files)
+        val_speeches.extend(files[num_train:end])
+        test_speeches.extend(files[end:])
     return train_speeches, val_speeches, test_speeches
 
 def get_noise_files(noise_path, noises, num_train=1, num_val=1):
-    assert num_train+num_val <= len(noises)
+    assert num_train + num_val <= len(noises)
     if noise_path[-1] != '/':
         noise_path += '/'
     noises = [noise_path + noise for noise in noises]
