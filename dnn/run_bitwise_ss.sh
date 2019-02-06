508ca61fee041b97ec4eb4ae54d3831a08d411a4
Ryley Higa <higa2@illinois.edu>
2019-02-06 14:10:05 -0600

    Changed experiment file for bitwise ss

--- dnn/run_bitwise_ss.sh
+++ dnn/run_bitwise_ss.sh
@@ -1,5 +1,5 @@
-python bitwise_ss.py -exp tanh -a relu -wb identity -ib identity -ub -e 150 -dropout 0.3
-python bitwise_ss.py -exp nocw_nocg -a relu -wb ste -ib ste -ub -lf net_tanh.model -e 150 -bnm 0.2 -lrd 0.90 -as
-python bitwise_ss.py -exp nocw_cg -a relu -wb clipped_ste -ib clipped_ste -ub -lf net_tanh.model -e 150 -bnm 0.2 -lrd 0.90 -as
-python bitwise_ss.py -exp cw_cg -a relu -wb clipped_ste -ib clipped_ste -ub -lf net_tanh.model -cw -e 150 -bnm 0.2 -lrd 0.90 -as
-python bitwise_ss.py -exp cw_nocg -a relu -wb ste -ib ste -ub -lf net_tanh.model -cw -e 150 -bnm 0.2 -lrd 0.90 -as
+python bitwise_ss.py -exp relu -a relu -wb identity -ib identity -ub -e 150 -dropout 0.3
+python bitwise_ss.py -exp cw_cg -a relu -wb clipped_ste -ib clipped_ste -ub -lf relu.model -cw -e 150 -bnm 0.2 -lrd 0.90 -lr 1e-4
+python bitwise_ss.py -exp cw_nocg -a relu -wb ste -ib ste -ub -lf relu.model -cw -e 150 -bnm 0.2 -lrd 0.90 -lr 1e-4
+python bitwise_ss.py -exp nocw_nocg -a relu -wb ste -ib ste -ub -lf relu.model -e 150 -bnm 0.2 -lrd 0.90 -lr 1e-4
+python bitwise_ss.py -exp nocw_cg -a relu -wb clipped_ste -ib clipped_ste -ub -lf relu.model -e 150 -bnm 0.2 -lrd 0.90 -lr 1e-4
