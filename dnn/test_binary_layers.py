5d1f04134e7f0649f2281ab170e373a826280d6c
Ryley Higa <ryley.higa2@gmail.com>
2019-02-12 03:45:10 -0600

    Created abstract binary class

--- dnn/test_binary_layers.py
+++ dnn/test_binary_layers.py
@@ -5,10 +5,10 @@ import torch.optim as optim
 import numpy as np
 from binary_layers import *
 
-class TestBinaryLayers(unittest.TestCase):
+class TestBitwiseLinear(unittest.TestCase):
     def setUp(self):
         # Create dataset
-        self.blinear = BitwiseLinear(3, 3, use_gate=False, scale_weights=True,
+        self.blinear = BitwiseLinear(3, 3, use_gate=False, scale_weights='average',
             binactiv=clipped_ste)
         linear_weight = 0.1 * torch.FloatTensor([
             [1, 2, 0],
@@ -21,48 +21,47 @@ class TestBinaryLayers(unittest.TestCase):
         gamma = torch.FloatTensor([10, 2, 100])
         self.scale.gamma = nn.Parameter(gamma)
 
+    def test_initalization(self):
+        self.assertTrue(self.blinear.bitwise)
+        self.assertTrue(self.blinear.gate is None)
+        self.assertTrue(self.blinear.scale_weights)
+
+    def test_drop_with_gate(self):
+        w = torch.FloatTensor([
+            [0.1, 0, 0],
+            [0, 0, 0.2],
+            [0, -0.1, 0]
+        ])
+        self.blinear.gate = nn.Parameter(torch.FloatTensor([
+            [1, -3, 5],
+            [-2, -4, 0.5],
+            [6, 7.5, -0.01],
+        ]))
+        w_hat = self.blinear.drop_weights()
+        self.assertTrue(torch.allclose(w, w_hat, rtol=0))
+
+        # we don't want test interferring with other tests
+        self.blinear.gate = None
+        self.assertTrue(self.blinear.gate is None)
+
+    def test_drop_with_beta(self):
+        w = torch.FloatTensor([
+            [0, 0.2, 0],
+            [0.3, 0, 0.2],
+            [0, 0, 0.2]
+        ])
+        self.blinear.beta = nn.Parameter(torch.tensor(0.15), requires_grad=False)
+        w_hat = self.blinear.drop_weights()
+        self.assertTrue(torch.allclose(w, w_hat, rtol=0))
+
+        # we don't want test interferring with other tests
+        self.blinear.beta = nn.Parameter(torch.tensor(0, dtype=torch.float),
+            requires_grad=False)
+        self.assertTrue(self.blinear.beta == 0)
+
     def test_nodrop(self):
-        x = torch.FloatTensor([
-            [0.2, -0.3, 0.5],
-            [-0.4, 0.1, 0.3],
-            [0.5, -0.15, 0.12]
-        ])
-        y_hat = drop_weights(x, gate=None, binactiv=clipped_ste, beta=0)
-        self.assertTrue(torch.all(torch.eq(x, y_hat)))
-
-    def test_drop_weights_with_beta(self):
-        x = torch.FloatTensor([
-            [0.2, -0.3, 0.5],
-            [-0.4, 0.1, 0.3],
-            [0.5, -0.15, 0.12]
-        ])
-        y = torch.FloatTensor([
-            [0, -0.3, 0.5],
-            [-0.4, 0, 0.3],
-            [0.5, 0, 0]
-        ])
-        beta = 0.25
-        y_hat = drop_weights(x, gate=None, binactiv=None, beta=beta)
-        self.assertTrue(torch.all(torch.eq(y, y_hat)))
-
-    def test_drop_weights_with_gate(self):
-        x = torch.FloatTensor([
-            [0.2, -0.3, 0.5],
-            [-0.4, 0.1, 0.3],
-            [0.5, -0.15, 0.12]
-        ])
-        y = torch.FloatTensor([
-            [0.2, -0.3, 0.5],
-            [0, 0, 0.3],
-            [0.5, -0.15, 0]
-        ])
-        gate = torch.FloatTensor([
-            [0.6, 0.34, 0.5],
-            [-0.35, -0.1, 0.3],
-            [0.5, 0.15, -0.12]
-        ])
-        y_hat = drop_weights(x, gate=gate, binactiv=clipped_ste, beta=0)
-        self.assertTrue(torch.all(torch.eq(y, y_hat)))
+        w_hat = self.blinear.drop_weights()
+        self.assertTrue(torch.allclose(self.blinear.weight, w_hat, rtol=0))
 
     def test_linear_scale_weights(self):
         '''
@@ -77,18 +76,35 @@ class TestBinaryLayers(unittest.TestCase):
             [0, 10, 10]
         ])
         y_hat = self.blinear(x)
-        print(y_hat)
         self.assertTrue(torch.all(torch.eq(y, y_hat)))
 
-    def test_no_binactiv(self):
+    def test_binarize_inputs(self):
         x = torch.FloatTensor([
             [0.2, -0.3, 0.5],
             [-0.4, 0.1, 0.3],
             [0.5, -0.15, 0.12]
         ])
-        x_hat, w_hat = binarize_weights_and_inputs(x, self.blinear.weight)
-        self.assertTrue(torch.all(torch.eq(x, x_hat[0])))
-        self.assertTrue(torch.all(torch.eq(self.blinear.weight, w_hat)))
+        y = torch.FloatTensor([
+            [1/3, -1/3, 1/3],
+            [-0.8/3, 0.8/3, 0.8/3],
+            [0.77/3, -0.77/3, 0.77/3]
+        ])
+        y_hat = self.blinear.binarize_inputs(x)
+        self.assertTrue(torch.allclose(y, y_hat[0], rtol=0))
+
+    def test_binarize_weights(self):
+        w = torch.FloatTensor([
+            [1, 1, 0],
+            [1, 1, 1],
+            [0, -1, 1]
+        ])
+        self.blinear.scale_weights = False
+        w_hat = self.blinear.binarize_weights()
+        self.assertTrue(torch.allclose(w, w_hat, rtol=0))
+
+        self.blinear.scale_weights = True
+        self.assertTrue(self.blinear.scale_weights)
+
 
     def test_scale_layer(self):
         x = torch.FloatTensor([
@@ -102,7 +118,7 @@ class TestBinaryLayers(unittest.TestCase):
             [5, -0.3, 12]
         ])
         y_hat = self.scale(x)
-        self.assertTrue(torch.all(torch.eq(y, y_hat)))
+        self.assertTrue(torch.allclose(y, y_hat, rtol=0))
 
 
 if __name__ == '__main__':
